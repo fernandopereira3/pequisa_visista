a42dee8296c055cec5175af2a2701ec00780a2eb
Fernando Pereira <39241138+fernandopereira3@users.noreply.github.com>
2022-11-29 16:15:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from rich import print
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://10.14.180.204:27017/')
+db = client.cpppac
+sentenciados = db.sentenciados
+
+st.title('Pesquisa')
+
+barra = st.sidebar
+
+escolha = barra.selectbox('O que voce quer pesquisar',['Preso','Visita'])
+matr = st.text_input('Qual a matricula a ser pesquisada')
+
+
+if st.button('Pesquisa'):
+    for matr in sentenciados.find({'matricula': matr}):
+      st.write('Nome: ', matr['nome'],',', matr['pavilhao'])
+
+
+#matr = input('Matricula a ser pesquisada: ')
+#
+#  print(matr['pavilhao'])
