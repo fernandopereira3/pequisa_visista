e5b659e06bf405aa85f2854aa70cd5f1e4f6ea2e
Fernando <fernandopereira-3@hotmail.com>
2025-02-12 16:59:39 -0300

    Bug da pesquisa e no add numeros

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ except Exception as e:
 #    except Exception as e:
 #        st.error('Timeout na conexão com o banco de dados remoto. Tente novamente.', icon='👎')
 
-st.title('Lista para o dia de Visita')
+st.title('Lista da Visita')
 
 # Inicializa a lista no session_state, se ainda não existir
 if 'sentenciados_lista' not in st.session_state:
@@ -62,39 +62,46 @@ def buscar_por_nome(nome):
     return list(sentenciados.find({'nome': {'$regex': padrao_nome}}))
 
 # Função para adicionar sentenciado à lista
-def adicionar_a_lista(matricula):
+def adicionar_a_lista(matricula, pet, homem, mulher, crianca):
     matricula_normalizada = re.compile(f".*{matricula}.*", re.IGNORECASE)
     sentenciado = sentenciados.find_one({'matricula': {'$regex': matricula_normalizada}})
-    pet = st.number_input('Quantidade de garrafas PET', min_value=0, max_value=10, value=0)
-    homem = st.number_input('Quantidade de HOMENS', min_value=0, max_value=10, value=0)
-    mulher = st.number_input('Quantidade de MULHERES', min_value=0, max_value=10, value=0)
-    crianca = st.number_input('Quantidade de CRIANCAS', min_value=0, max_value=10, value=0)
+    
+    if not sentenciado:
+        st.warning('Sentenciado não encontrado!')
+        return
+ 
+    # Verifica se a matrícula já está na lista antes de adicionar
+    if any(s['matricula'] == sentenciado['matricula'] for s in st.session_state['sentenciados_lista']):
+        st.info('Esta matrícula já está na lista!')
+        return
+    
+    st.session_state['sentenciados_lista'].append({
+        'matricula': sentenciado['matricula'],
+        'nome': sentenciado['nome'],
+        'PET': pet,
+        'HOMEM': homem,
+        'MULHER': mulher,
+        'CRIANCA': crianca,
+    })
+    
+    if st.session_state['sentenciados_lista']:
+        st.toast(f'Matrícula {sentenciado["matricula"]} adicionada à lista com sucesso!')
+        df = pd.DataFrame(st.session_state['sentenciados_lista'])
+        st.dataframe(df)
 
-    if sentenciado:
-        if (homem > 0 or mulher > 0):
-            if sentenciado['matricula'] not in [s['matricula'] for s in st.session_state['sentenciados_lista']]:
-                st.session_state['sentenciados_lista'].append({
-                    'matricula': sentenciado['matricula'],
-                    'nome': sentenciado['nome'],
-                    'PET': pet,
-                    'HOMEM': homem,
-                    'MULHER': mulher,
-                    'CRIANCA': crianca,
-                })
-                st.success(f'Matrícula {sentenciado["matricula"]} adicionada à lista com sucesso!')
-            else:
-                st.info('Esta matrícula já está na lista!')
-        else:
-            st.warning('É necessário ter pelo menos um visitante (homem ou mulher) para adicionar à lista!')
-    else:
-        st.write(sentenciado)
-        st.warning('Sentenciado não encontrado! ')
 # Escolha do tipo de busca
+#tipo_busca = st.radio('Pesquisar por:', ['Matrícula', 'Nome'])
 
-tipo_busca = st.radio('Pesquisar por:', ['Matrícula', 'Nome'])
 
-# Campo de entrada dinâmico baseado na escolha
-txt_d_pesquisa = st.text_input('Digite a informação para pesquisa')
+coluna = st.columns(1)
+txt_d_pesquisa = coluna[0].text_input('Digite a quem esta procurando')
+
+col1, col2 = st.columns(2)
+
+pet = col1.number_input('Quantidade de GARRAFAS PETS', min_value=0, max_value=10, value=0)
+homem = col1.number_input('Quantidade de HOMENS', min_value=0, max_value=10, value=0)
+mulher = col2.number_input('Quantidade de MULHERES', min_value=0, max_value=10, value=0)
+crianca = col2.number_input('Quantidade de CRIANCAS', min_value=0, max_value=10, value=0)
 
 # Criando colunas para botões
 col1, col2, col3, col4 = st.columns(4)
@@ -102,53 +109,39 @@ col1, col2, col3, col4 = st.columns(4)
 # Botão de Pesquisa
 if col1.button('Pesquisar'):
     if txt_d_pesquisa:
-        if tipo_busca == 'Matrícula':
-            resultados = buscar_por_matricula(txt_d_pesquisa)
-        else:
+        resultados = buscar_por_matricula(txt_d_pesquisa)
+        if not resultados:
             resultados = buscar_por_nome(txt_d_pesquisa)
-
         if resultados:
-            for sentenciados in resultados:
-                st.write(f"**Nome:** {sentenciados['nome']} --- **Matr:** {sentenciados['matricula']} --- {sentenciados.get('pavilhao', 'N/A')}")
+            for sentenciado in resultados:
+                st.write(f"**Nome:** {sentenciado['nome']} --- **Matr:** {sentenciado['matricula']} --- {sentenciado.get('pavilhao', 'N/A')}")
                 st.markdown('---')
         else:
-            st.warning('Debug')
-            st.write(buscar_por_matricula(txt_d_pesquisa))
-            st.write(f'MATRICULA NORMALIZADA {normalizar_matricula(txt_d_pesquisa)}')
-            st.write(list(sentenciados.find({'matricula': {'$regex': txt_d_pesquisa}})))
+            st.json(buscar_por_matricula(resultados))
+            st.write(buscar_por_matricula(resultados))
             st.warning(f'O sentenciado com {txt_d_pesquisa} não foi encontrado.')
     else:
         st.warning('Digite nome ou matricula para pesquisar.')
 
 # Botão Adicionar à Lista (apenas para matrícula)
 if col2.button('Add matriculas'):
-    if txt_d_pesquisa != False:
-        adicionar_a_lista(txt_d_pesquisa)
+    if normalizar_matricula(txt_d_pesquisa):
+        adicionar_a_lista(txt_d_pesquisa, pet, homem, mulher, crianca)
     else:
-        st.write(txt_d_pesquisa)
         st.warning('Somente matrículas podem ser adicionadas na lista !.')
 
+# Botão Limpar Lista
 if col4.button('Limpar Lista'):
     st.session_state['sentenciados_lista'] = []
     st.success('Lista de sentenciados foi limpa!')
-
-# Exibição da lista de sentenciados
-if st.session_state['sentenciados_lista']:
-    #st.markdown('---')  # Linha divisória
-    st.subheader('Matrículas na Lista:')
     
-    # Exibe os dados como tabela
-    df = pd.DataFrame(st.session_state['sentenciados_lista'])
-    st.dataframe(df)
-
-    # Multiselect para selecionar matrículas para remover
+# Botão para remover as matrículas
+if col3.button('Remover Matrículas'):
+ # Multiselect para selecionar matrículas para remover
     matriculas_para_remover = st.multiselect(
         'Selecione as matrículas para remover:',
         [s['matricula'] for s in st.session_state['sentenciados_lista']]
     )
-
-# Botão para remover as matrículas selecionadas
-if col3.button('Remover Matrículas'):
     if matriculas_para_remover:
         st.session_state['sentenciados_lista'] = [
             s for s in st.session_state['sentenciados_lista']
