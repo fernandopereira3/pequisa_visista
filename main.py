5aaabf2d0d099424e66b0bd64190cd9c457fa6a5
Fernando <fernandopereira-3@hotmail.com>
2025-02-11 21:47:09 -0300

    ativacao de github actions

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ if col3.button('Remover Matrículas'):
         st.info('Nenhuma matrícula selecionada para remover.')
 
 
-# Workflow
+# Workflow para salvar a lista de sentenciados
 
 # Exibição da lista de sentenciados
 #if st.session_state['sentenciados_lista']:
